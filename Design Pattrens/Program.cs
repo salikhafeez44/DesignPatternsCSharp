68e7af00758f98706e48def7beb9f2915fc3f15e
salik Hafeez <salikhafeez44@gmail.com>
2022-02-18 16:23:54 -0600

    Singleton Pattern

---
+++ Design Pattrens/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Design_Pattrens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj1 = Singleton.Instance;
+            Singleton obj2 = Singleton.Instance;
+
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            Console.WriteLine();
+
+
+             
+        }
+    }
+}
